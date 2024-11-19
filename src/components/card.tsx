a4d22e9b3bbd8c98a926e967b6038c07713ff16a
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-11-18 19:43:25 -0600

    Implement skeleton of 2 sections

---
+++ src/components/card.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+    return (
+        <div className="bg-white px-2 py-4 rounded-lg shadow-lg h-fit w-1/2 flex-wrap text-black">
+            <h1 className="text-2xl font-semibold mx-2">Title</h1>
+            <p className="mx-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita illo architecto itaque veritatis. Quaerat sit, error aperiam molestias soluta quo deserunt suscipit officiis, temporibus nemo saepe sapiente voluptas doloremque placeat.</p>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,32 @@
+import Card from "../components/card";
+
 export default function Index() {
-    return <h1 className="text-3xl font-bold font-mono">Hello World</h1>
+    return (
+        <div className="overflow-x-hidden my-0">
+            <div className="bg-fellaRed h-screen w-screen  flex flex-row relative">
+                <div className="basis-2/3 bg-red-500 h-screen flex">
+                    <div className="my-auto ml-12 text-white">
+                        <h1 className="text-9xl font-semibold">Hello,</h1>
+                        <h1 className="text-9xl font-semibold">World</h1>
+
+                        <p className="text-2xl font-mono mt-5 ml-2">User-Agent: Jorge Hernandez 🇭🇳</p>
+                        <p className="text-2xl font-mono ml-2">Accept-Language: es, en, de</p>
+                        <p className="text-2xl font-mono ml-2">Accept: dev/backend, lang/python (and more...)</p>
+                    </div>
+                </div>
+
+                <div className="bg-green-400 flex-1">
+                    imagen
+                </div>
+            </div>
+
+            <div className="bg-black text-white mt-0">
+                <h1 className="text-4xl font-semibold mt-2 text-center">Projects</h1>
+
+                <div className="grid grid-cols-3 gap-4">
+                    <Card />
+                </div>
+            </div>
+        </div>
+    )
 }
