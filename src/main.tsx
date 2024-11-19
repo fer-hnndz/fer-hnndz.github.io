3958422f700ea42f274b13ba9b7287c2818359c4
fer-hnndz <65425622+fer-hnndz@users.noreply.github.com>
2024-11-18 18:58:10 -0600

    Sort project structure and add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Index from './pages/index.tsx'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Index />
   </StrictMode>,
 )
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Index() {
+    return <h1 className="text-3xl font-bold font-mono">Hello World</h1>
+}
